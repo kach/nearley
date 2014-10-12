61b6dd4eef90b385574e38fe13bb40490c9cf82a
Hardmath123 <hardmath123@gmail.com>
2014-10-12 13:57:31 -0700

    comments... comments everywhere\!

--- examples/calculator/calculator.js
+++ examples/calculator/calculator.js
@@ -1,21 +1,33 @@
+// This is an example of how to use a nearley-made grammar.
 var PROMPT = "> ";
 
+// This is where the action is.
 function runmath(s) {
     var ans;
-    try {
-        ans = new require("./calc-grammar.js")().feed(s);
+    try {// We want to catch parse errors and die appropriately
+
+        // Make a parser and feed the input
+        ans = new require("./calc-grammar.js")()
+            .feed(s);
+        
+        // Check if there are any results
         if (ans.results.length) {
             return ans.results[0].toString();
         } else {
-            var out = new Array(PROMPT.length + s.length).join("-") + "^  Error.";
+            // This means the input is incomplete.
+            var out = "Error: incomplete input, parse failed. :(";
             return out;
         }
     } catch(e) {
+        // Panic in style, by graphically pointing out the error location.
         var out = new Array(PROMPT.length + e.offset + 1).join("-") + "^  Error.";
+        //                                  --------
+        //                                         ^ This comes from nearley!
         return out;
     }
 }
 
+// node readline gunk. Nothing too exciting.
 var readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
 
