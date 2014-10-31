cf117b40e618b0df00193fdaf4ccef5b18f7261d
rwindelz <rwindelz@gmail.com>
2014-10-30 21:54:49 -0700

    example grammars compiled

---
+++ examples/js/AycockHorspool.js
@@ -0,0 +1,18 @@
+// Generated automatically by nearley
+(function () {
+function id(x) {return x[0]; }
+var grammar = {
+    ParserRules: [
+    {"name": "S", "symbols": ["A", "A", "A", "A"]},
+    {"name": "A", "symbols": [{"literal":"a"}]},
+    {"name": "A", "symbols": ["E"]},
+    {"name": "E", "symbols": []}
+]
+  , ParserStart: "S"
+}
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+   module.exports = grammar;
+} else {
+   window.grammar = grammar;
+}
+})();
