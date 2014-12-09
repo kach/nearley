55150b018ac9b9c6f22eb1f763c17cdb0809e061
JacobEdelman <edelman.jd@gmail.com>
2014-12-08 21:11:34 -0500

    fixing

--- test/profile.js
+++ test/profile.js
@@ -19,6 +19,8 @@ function profile(n, type) {
 	}
 	var starttime = process.hrtime();
 	var startmemory = process.memoryUsage().heapUsed;
+	console.log('okau');
+	console.log(parserGrammar);
 	var p = new nearley.Parser(parserGrammar.ParserRules, parserGrammar.ParserStart).feed(test);
 	console.assert(p.results[0]);
 	switch (type) {
