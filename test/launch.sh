067e571e0430c875191bb9412b637378dc742707
corwin <corwin.amber@gmail.com>
2016-01-22 13:58:42 -0500

    Fixed bug where multiple identical parses were created for an input in an unambiguous grammar in some situation involving nullable nonterminals (issue #85).

--- test/launch.sh
+++ test/launch.sh
@@ -14,13 +14,18 @@ time bin/nearleyc.js test/indentation.ne > /dev/null
 echo "Testing percent bug..."
 bin/nearleyc.js test/percent.ne > /dev/null
 
+echo "Testing nullable whitespace bug..."
+bin/nearleyc.js test/whitespace.ne -o test/whitespace.js
+bin/nearley-test.js test/whitespace.js -i "(x)" | sed -e '1,/Parse results:/d' \
+    | diff - test/whitespace.expected || echo "[ FAILED ]"
+
 if ! type "coffee" > /dev/null; then
     echo "Coffeescript not installed, skipping coffeescript test!";
 else
     echo "Testing coffeescript template";
     bin/nearleyc.js test/coffeescript-test.ne -o test/tmp.coffeescript-test.coffee;
     coffee -c test/tmp.coffeescript-test.coffee;
-    nearley-test test/tmp.coffeescript-test.js -i "ABCDEFZ12309" > /dev/null;
+    bin/nearley-test.js test/tmp.coffeescript-test.js -i "ABCDEFZ12309" > /dev/null;
 fi
 
 echo "Done with all tests."
