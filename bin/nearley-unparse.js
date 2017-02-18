932c59a5fc2b87dfa46dce4b47997ec2b075f7a2
Hardmath123 <hardmath123@gmail.com>
2017-02-17 23:46:04 -0800

    making the unparser tolerable

--- bin/nearley-unparse.js
+++ bin/nearley-unparse.js
@@ -21,6 +21,11 @@ var opts = nomnom
         help: 'The number of samples to generate (separated by \\n).',
         default: 1
     })
+    .option('depth', {
+        abbr: 'd',
+        help: 'The depth bound of each sample. Defaults to -1, which means "unbounded".',
+        default: -1
+    })
     .option('out', {
         abbr: 'o',
         help: "File to output to (defaults to stdout)",
@@ -38,6 +43,73 @@ var opts = nomnom
 var output = opts.out ? fs.createWriteStream(opts.out) : process.stdout;
 
 var grammar = new require(require('path').resolve(opts.file));
+
+
+function gen2(grammar, name, depth) {
+    // I guess you could call this the second-generation generator.
+    // All it does is bound its output by a certain depth without having to
+    // backtrack. It doesn't give guarantees on being uniformly random, but
+    // that's doable if we *really* need it (by converting min_depth_rule, a
+    // predicate, into something that counts the number of trees of depth d).
+
+    var rules = grammar.ParserRules;
+    var min_depths_rule = [];
+
+    function synth_nt(name, depth) {
+        var good_rules = [];
+        for (var i=0; i<rules.length; i++) {
+            min_depths_rule = [];
+            if (rules[i].name === name && min_depth_rule(i, []) < depth) {
+                good_rules.push(i);
+            }
+        }
+
+        var r = good_rules[Math.floor(Math.random()*good_rules.length)];
+        return synth_rule(r, depth);
+    }
+    function synth_rule(idx, depth) {
+        var ret = "";
+        for (var i=0; i<rules[idx].symbols.length; i++) {
+            var tok = rules[idx].symbols[i];
+            if (typeof(tok) === 'string') {
+                ret += synth_nt(tok, depth-1);
+            } else if (tok.test) {
+                ret += new randexp(tok).gen();
+            } else if (tok.literal) {
+                ret += tok.literal;
+            }
+        }
+        return ret;
+    }
+    function min_depth_nt(name, visited) {
+        if (visited.indexOf(name) !== -1) {
+            return +Infinity;
+        }
+        var d = +Infinity;
+        for (var i=0; i<rules.length; i++) {
+            if (rules[i].name === name) {
+                d = Math.min(d, min_depth_rule(i, [name].concat(visited)));
+            }
+        }
+        return d;
+    }
+    function min_depth_rule(idx, visited) {
+        if (min_depths_rule[idx] !== undefined) return min_depths_rule[idx];
+
+        var d = 1;
+        for (var i=0; i<rules[idx].symbols.length; i++) {
+            var tok = rules[idx].symbols[i];
+            if (typeof(tok) === 'string') {
+                d = Math.max(d, 1+min_depth_nt(tok, visited));
+            }
+        }
+        min_depths_rule[idx] = d;
+        return d;
+    }
+
+    var ret = synth_nt(name, depth);
+    return ret;
+}
 
 function gen(grammar, name) {
     var stack = [name];
@@ -72,6 +144,10 @@ function gen(grammar, name) {
 }
 
 for (var i=0; i<parseInt(opts.count); i++) {
-    gen(grammar, opts.start ? opts.start : grammar.ParserStart);
-    output.write("\n");
+    if (opts.depth === -1) {
+        gen(grammar, opts.start ? opts.start : grammar.ParserStart);
+    } else {
+        output.write(gen2(grammar, opts.start ? opts.start : grammar.ParserStart, opts.depth));
+    }
+    if (opts.count > 1) output.write("\n");
 }
