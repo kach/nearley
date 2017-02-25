6fec19ce1f0cef68b4ffd8d8660d90438ff56194
tjvr <tim@tjvr.org>
2017-02-25 14:04:38 +0000

    tidy up as per review comments :-)

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,13 +1,7 @@
 
-var fs = require('fs');
 var path = require('path');
-var child_process = require('child_process')
 
 var Benchmark = require('benchmark');
-var colors = require('colors/safe');
-
-var formatNumber = Benchmark.formatNumber;
-var suite = new Benchmark.Suite();
 
 var Parser = require('../lib/nearley.js').Parser;
 var shared = require('./_shared.js');
@@ -15,20 +9,12 @@ var compile = shared.compile
   , read = shared.read;
 
 
-// Define benchmarks
+// For making tests
 
-function addTest(parserName, parser, exampleInputs) {
-    exampleInputs.forEach(function(inputPath) {
-        var input;
-        var exampleName;
-        if (/^test\//.test(inputPath)) {
-            input = read(inputPath);
-            exampleName = path.basename(inputPath);
-        } else {
-            input = inputPath;
-            exampleName = '"' + input + '"';
-        }
-        suite.add(parserName + ': parse ' + exampleName, function() {
+function addTest(parserName, parser, examples) {
+    examples.forEach(function(example) {
+        var input = example.source;
+        suite.add(parserName + ' ' + example.name, function() {
             parser(input);
         });
     })
@@ -54,53 +40,49 @@ function makeParser(neFile) {
     return parse;
 }
 
+function Example(name, source) {
+    this.name = name;
+    this.source = source;
+}
 
-// TODO benchmark compile
+Example.read = function(filename) {
+  return new Example(path.basename(filename), read(filename));
+};
 
+// Define benchmarks
 
-addTest('calculator example', makeParser('examples/calculator/arithmetic.ne'), [
-    '2 + 3 * 42 - sin(0.14)',
-    'ln (3 + 2*(8/e - sin(pi/5)))',
+var suite = new Benchmark.Suite();
+
+addTest('calculator', makeParser('examples/calculator/arithmetic.ne'), [
+    new Example('arithmetic1', '2 + 3 * 42 - sin(0.14)'),
+    new Example('arithmetic2', 'ln (3 + 2*(8/e - sin(pi/5)))'),
 ]);
 
-addTest('json example', makeParser('examples/json.ne'), [
-    'test/test1.json',
-    'test/test2.json',
+addTest('json', makeParser('examples/json.ne'), [
+    Example.read('test/test1.json'),
+    Example.read('test/test2.json'),
 ]);
 
 /*
 addTest('native JSON.parse', JSON.parse, [
-   'test/test1.json',
-   'test/test2.json',
+   Example.read('test/test1.json'),
+   Example.read('test/test2.json'),
 ])
 */
+
+// TODO benchmark compile
 
 
 // Run & report results
 
-var longestName = Math.max.apply(null, suite.map(function(bench) {
-    return bench.name.length;
-}));
-function padName(x) {
-    while (x.length < longestName) {
-        x += ' ';
-    }
-    return x;
-}
-
 suite.on('cycle', function(event) {
     var bench = event.target;
-    var stats = bench.stats;
-    var hz = bench.hz; // Hz -- ops per sec
-    var pm = '\xb1';
-
     if (bench.error) {
-        console.log(colors.red("✘"), bench.name);
-        console.log(colors.red(bench.error.stack));
+        console.log('  ✘ ', bench.name);
+        console.log(bench.error.stack);
         console.log('');
     } else {
-        var opsPerSec = formatNumber(hz.toFixed(hz < 100 ? 2 : 0)) + ' ops/sec ' + pm + stats.rme.toFixed(2) + '%';
-        console.log(colors.green("✔"), padName(bench.name), colors.blue(opsPerSec));
+        console.log('  ✔ ' + bench)
     }
 })
 .on('complete', function() {
