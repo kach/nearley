741dfd14ed12befd32af6d86a826865cd6e8b928
Tim Radvan <tim@tjvr.org>
2017-08-09 11:19:32 +0100

    Remove build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-echo "Building nearley..."
-./bin/nearleyc.js ./lib/nearley-language-bootstrapped.ne -o grammar.tmp
-echo "Deleting temp file..."
-mv grammar.tmp ./lib/nearley-language-bootstrapped.js
-echo "Running tests..."
-test/launch.sh
