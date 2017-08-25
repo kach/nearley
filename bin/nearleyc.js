c44fe0c54a02a537cb42b93838e367f6ebaae55d
tjvr <tim@tjvr.org>
2017-08-25 12:12:52 +0100

    nearleyc should really use fromCompiled

--- bin/nearleyc.js
+++ bin/nearleyc.js
@@ -39,8 +39,8 @@ var opts = nomnom
 var input = opts.file ? fs.createReadStream(opts.file) : process.stdin;
 var output = opts.out ? fs.createWriteStream(opts.out) : process.stdout;
 
-var parserGrammar = require('../lib/nearley-language-bootstrapped.js');
-var parser = new nearley.Parser(parserGrammar.ParserRules, parserGrammar.ParserStart);
+var parserGrammar = nearley.Grammar.fromCompiled(require('../lib/nearley-language-bootstrapped.js'));
+var parser = new nearley.Parser(parserGrammar);
 var generate = require('../lib/generate.js');
 var lint = require('../lib/lint.js');
 
