ead0b6f210e29d4bf9a03719f4866b2f1b828f45
tjvr <tim@tjvr.org>
2018-03-04 17:19:30 +0000

    Add benchmarks for parsing nearley files

--- test/benchmark.js
+++ test/benchmark.js
@@ -29,7 +29,28 @@ function makeParser(neFile) {
 
 // Define benchmarks
 
-suite('calculator', () => {
+/*suite('nearley: parse scannerless-nearley.ne', () => {
+  //const nearleyGrammar = read('lib/nearley-language-bootstrapped.ne')
+  const nearleyGrammar = read('test/grammars/scannerless-nearley.ne')
+
+  const parseLexer = makeParser('lib/nearley-language-bootstrapped.ne')
+  benchmark('current', () => parseLexer(nearleyGrammar))
+
+  const parseScannerless = makeParser('test/grammars/scannerless-nearley.ne')
+  benchmark('scannerless', () => parseScannerless(nearleyGrammar))
+})
+
+suite('nearley: parse tosh.ne', () => {
+  const toshGrammar = read('examples/tosh.ne')
+
+  const parseLexer = makeParser('lib/nearley-language-bootstrapped.ne')
+  benchmark('current', () => parseLexer(toshGrammar))
+
+  const parseScannerless = makeParser('test/grammars/scannerless-nearley.ne')
+  benchmark('scannerless', () => parseScannerless(toshGrammar))
+})*/
+
+suite('calculator: parse', () => {
   const exampleFile = 'ln (3 + 2*(8/e - sin(pi/5)))'
 
   const parse = makeParser('examples/calculator/arithmetic.ne')
@@ -37,7 +58,7 @@ suite('calculator', () => {
 
 })
 
-suite('json', () => {
+suite('json: parse sample1k', () => {
   const jsonFile = read('test/grammars/sample1k.json')
 
   const parse = makeParser('examples/json.ne')
@@ -47,7 +68,7 @@ suite('json', () => {
 
 })
 
-suite('tosh', () => {
+suite('tosh: parse', () => {
   const toshFile = 'set foo to 2 * e^ of ( foo * -0.05 + 0.5) * (1 - e ^ of (foo * -0.05 + 0.5))'
 
   const parse = makeParser('examples/tosh.ne')
