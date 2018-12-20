6a7bd3239a7cc270dc57a33402903dc786835fa7
kach <hardmath123@gmail.com>
2018-12-20 15:57:14 -0800

    replace nomnom with commander

--- bin/nearley-unparse.js
+++ bin/nearley-unparse.js
@@ -2,51 +2,25 @@
 
 var fs = require('fs');
 var nearley = require('../lib/nearley.js');
-var nomnom = require('nomnom');
+var opts = require('commander');
 var randexp = require('randexp');
 var Unparse = require('../lib/unparse.js');
 
-var opts = nomnom
-    .script('nearley-unparse')
-    .option('file', {
-        position: 0,
-        help: "A grammar .js file",
-        required: true,
-    })
-    .option('start', {
-        abbr: 's',
-        help: "An optional start symbol (if not provided then use the parser start symbol)",
-    })
-    .option('count', {
-        abbr: 'n',
-        help: 'The number of samples to generate (separated by \\n).',
-        default: 1
-    })
-    .option('depth', {
-        abbr: 'd',
-        help: 'The depth bound of each sample. Defaults to -1, which means "unbounded".',
-        default: -1
-    })
-    .option('out', {
-        abbr: 'o',
-        help: "File to output to (defaults to stdout)",
-    })
-    .option('version', {
-        abbr: 'v',
-        flag: true,
-        help: "Print version and exit",
-        callback: function() {
-            return require('../package.json').version;
-        }
-    })
-    .parse();
+var version = require('../package.json').version;
+opts.version(version, '-v, --version')
+    .arguments('<file.js>')
+    .option('-s, --start [name]', 'An optional start symbol (if not provided then use the parser start symbol)', false)
+    .option('-n, --count [n]', 'The number of samples to generate (separated by \\n).', 1)
+    .option('-d, --depth [n]', 'The depth bound of each sample. Defaults to -1, which means "unbounded".', -1)
+    .option('-o, --out [filename]', 'File to output to (defaults to stdout)')
+    .parse(process.argv);
 
 var output = opts.out ? fs.createWriteStream(opts.out) : process.stdout;
 
-var grammar = new require(require('path').resolve(opts.file));
+var grammar = new require(require('path').resolve(opts.args[0]));
 
 // the main loop
 for (var i=0; i<parseInt(opts.count); i++) {
-    output.write(Unparse(grammar, opts.start ? opts.start : grammar.ParserStart, (opts.depth === -1) ? null : opts.depth));
-    if (opts.count > 1) output.write("\n");
+    output.write(Unparse(grammar, opts.start ? opts.start : grammar.ParserStart, (opts.depth === -1) ? null : parseInt(opts.depth)));
+    if (parseInt(opts.count) > 1) output.write("\n");
 }
--- bin/nearleyc.js
+++ bin/nearleyc.js
@@ -2,48 +2,22 @@
 
 var fs = require('fs');
 var nearley = require('../lib/nearley.js');
-var nomnom = require('nomnom');
+var opts = require('commander');
 var Compile = require('../lib/compile.js');
 var StreamWrapper = require('../lib/stream.js');
 
-var opts = nomnom
-    .script('nearleyc')
-    .option('file', {
-        position: 0,
-        help: "An input .ne file (if not provided then read from stdin)",
-    })
-    .option('out', {
-        abbr: 'o',
-        help: "File to output to (defaults to stdout)",
-    })
-    .option('export', {
-        abbr: 'e',
-        help: "Variable to set the parser to",
-        default: "grammar"
-    })
-    .option('quiet', {
-        abbr: 'q',
-        flag: true,
-        help: "Suppress the linter."
-    })
-    .option('nojs', {
-        flag: true,
-        default: false,
-        help: "Don't compile postprocessors (for testing)."
-    })
-    .option('version', {
-        abbr: 'v',
-        flag: true,
-        help: "Print version and exit",
-        callback: function() {
-            return require('../package.json').version;
-        }
-    })
-    .parse();
-
 var version = require('../package.json').version;
 
-var input = opts.file ? fs.createReadStream(opts.file) : process.stdin;
+opts.version(version, '-v, --version')
+    .arguments('<file.ne>')
+    .option('-o, --out [filename.js]', 'File to output to (defaults to stdout)', false)
+    .option('-e, --export [name]', 'Variable to set parser to', 'grammar')
+    .option('-q, --quiet', 'Suppress linter')
+    .option('--nojs', 'Do not compile postprocessors')
+    .parse(process.argv);
+
+
+var input = opts.args[0] ? fs.createReadStream(opts.args[0]) : process.stdin;
 var output = opts.out ? fs.createWriteStream(opts.out) : process.stdout;
 
 var parserGrammar = nearley.Grammar.fromCompiled(require('../lib/nearley-language-bootstrapped.js'));
